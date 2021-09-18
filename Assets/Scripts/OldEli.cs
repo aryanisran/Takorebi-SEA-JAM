fc1bf089692e384a94644344776266b26684f0c1
Aryan Isran <aryanisran@gmail.com>
2021-09-18 22:00:59 +0800

    Seperate Granny abilities into new script

---
+++ Assets/Scripts/OldEli.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldEli : MonoBehaviour
+{
+    PlayerController player;
+    public bool isBuilding, prevFrame, canBuild;
+    public GameObject guidePrefab, platformPrefab;
+    GameObject guide, platform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canBuild = true;
+        player = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Build Platform
+        //Make the guide when we press X
+        if (Input.GetKeyDown(KeyCode.X) && !isBuilding && canBuild)
+        {
+            isBuilding = true;
+            prevFrame = true;
+            //Round the x and y of the platform to the nearest 0.5 so it's in line with the grid
+            float roundedX = Mathf.Round((transform.position.x) * 2) / 2;
+            float roundedY = Mathf.Round((transform.position.y) * 2) / 2;
+            guide = Instantiate(guidePrefab, new Vector3(roundedX, roundedY, 0), Quaternion.identity);
+        }
+        if (isBuilding)
+        {
+            //Move the guide when pressing arrow buttons
+            Vector3 inputDir;
+            inputDir = Vector3.zero;
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                inputDir += Vector3.right;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                inputDir += Vector3.left;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                inputDir += Vector3.up;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                inputDir += Vector3.down;
+            }
+            if (Vector3.Distance(guide.transform.position + inputDir * 0.25f, transform.position) < 1.5)
+            {
+                guide.transform.position += inputDir * 0.25f;
+            }
+            //Finalize building when we press X again
+            if (Input.GetKeyDown(KeyCode.X) && !prevFrame)
+            {
+                canBuild = false;
+                Destroy(Instantiate(platformPrefab, guide.transform.position, Quaternion.identity), 5f);
+                StartCoroutine(WaitPlatformCo());
+                //Store a temporary reference to the guide so we don't delete the guide stored in our global variable, just in case it gives us null refernce issues
+                GameObject temp = guide;
+                guide = null;
+                Destroy(temp);
+                isBuilding = false;
+            }
+            //This is to make sure we don't finalize building on the same frame we first pressed X
+            if (prevFrame && !Input.GetKey(KeyCode.X))
+            {
+                prevFrame = false;
+            }
+        }
+        #endregion
+    }
+
+
+    IEnumerator WaitPlatformCo()
+    {
+        yield return new WaitForSeconds(5f);
+        canBuild = true;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,7 @@ public class PlayerController : MonoBehaviour
     float maxJumpTime;
     [SerializeField] LayerMask realGround;
 
-    bool isBuilding, prevFrame, canBuild;
-    public GameObject guidePrefab, platformPrefab;
-    GameObject guide, platform;
+    OldEli oldEli;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,69 +22,15 @@ public class PlayerController : MonoBehaviour
         sr = GetComponentInChildren<SpriteRenderer>();
         anim = GetComponentInChildren<Animator>();
         maxJumpTime = jumpTime;
-        canBuild = true;
+
+        oldEli = GetComponent<OldEli>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        #region Build Platform
-        //Make the guide when we press X
-        if (Input.GetKeyDown(KeyCode.X) && !isBuilding && canBuild)
-        {
-            isBuilding = true;
-            prevFrame = true;
-            //Round the x and y of the platform to the nearest 0.5 so it's in line with the grid
-            float roundedX = Mathf.Round((transform.position.x) * 2) / 2;
-            float roundedY = Mathf.Round((transform.position.y) * 2) / 2;
-            guide = Instantiate(guidePrefab, new Vector3(roundedX, roundedY, 0), Quaternion.identity);
-        }
-        if (isBuilding)
-        {
-            //Move the guide when pressing arrow buttons
-            Vector3 inputDir;
-            inputDir = Vector3.zero;
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                inputDir += Vector3.right;
-            }
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                inputDir += Vector3.left;
-            }
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                inputDir += Vector3.up;
-            }
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                inputDir += Vector3.down;
-            }
-            if(Vector3.Distance(guide.transform.position + inputDir * 0.25f , transform.position) < 1.5)
-            {
-                guide.transform.position += inputDir * 0.25f;
-            }
-            //Finalize building when we press X again
-            if (Input.GetKeyDown(KeyCode.X) && !prevFrame)
-            {
-                canBuild = false;
-                Destroy(Instantiate(platformPrefab, guide.transform.position, Quaternion.identity), 5f);
-                StartCoroutine(WaitPlatformCo());
-                //Store a temporary reference to the guide so we don't delete the guide stored in our global variable, just in case it gives us null refernce issues
-                GameObject temp = guide;
-                guide = null;
-                Destroy(temp);
-                isBuilding = false;
-            }
-            //This is to make sure we don't finalize building on the same frame we first pressed X
-            if (prevFrame && !Input.GetKey(KeyCode.X))
-            {
-                prevFrame = false;
-            }
-        }
-        #endregion
         #region Movement
-        if (!isBuilding)
+        if (!oldEli.isBuilding)
         {
             //Take arrow key input and store it in Vector2
             moveInput.x = Input.GetAxis("Horizontal") * moveSpeed;
@@ -156,17 +100,11 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         //Set movement based on input
-        if (!isBuilding)
+        if (!oldEli.isBuilding)
         {
             rb.velocity = new Vector3(moveInput.x, rb.velocity.y, moveInput.y);
         }
         //Read vertical speed and send it to animator
         anim.SetFloat("dY", rb.velocity.y);
-    }
-
-    IEnumerator WaitPlatformCo()
-    {
-        yield return new WaitForSeconds(5f);
-        canBuild = true;
     }
 }
